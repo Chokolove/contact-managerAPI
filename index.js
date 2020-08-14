6cb980907e681fb0271b0c0364b4231698f21ac2
chokolove <carlosll_24@hotmail.com>
2020-08-14 11:57:38 -0500

    port change

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ fastify.register(require('fastify-cors'), {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000)
+    await fastify.listen(4000)
     fastify.log.info(`server listening on ${fastify.server.address().port}`)
   } catch (err) {
     fastify.log.error(err)
